00b1013075bfb9815a8a13db8d4f89affdf3e78c
David Knaack <davidkna@me.com>
2012-09-22 12:31:03 +0200

    improved ui & update jquery
    -new ui
    -update jquery
    -loading indicator
    -bugfixes

--- show.js
+++ show.js
@@ -3,6 +3,12 @@ $(function ($) {
 		var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
 		return decodeURIComponent(result && result[1] || "");
 	}
+
+	$('.loading').attr('src', chrome.extension.getURL('images/ajax-loader.gif'));
+	$('.navbar a').each(function() {
+		$(this).attr('href', chrome.extension.getURL($(this).attr('href')));
+	});
+
 	var url = localStorage['wikiURL'];
 	if (url[url.length - 1] != '/') {
 		url += '/';
@@ -10,29 +16,31 @@ $(function ($) {
 	var a = document.createElement('a');
 	a.href = url;
 	var base = a.protocol + '//' + a.hostname;
+	$('base').attr('href', base);
 	$.getJSON(url + 'w/api.php', {
 		'action': 'parse',
 		'format': 'json',
 		'page': getUrlVar("s"),
 		'prop': 'text'
 	}, function (response) {
-		$('#cnt').html(response.parse.text['*'].replace(new RegExp('src="//', 'g'), 'src="' + a.protocol + '//').replace(new RegExp('href="//', 'g'), 'src="' + a.protocol + '//').replace(new RegExp('src="/', 'g'), 'src="' + base + '/').replace(new RegExp('href="/', 'g'), 'href="' + base + '/'));
-		$('#cnt').prepend('<h1>' + getUrlVar('s') + '</h1>\n');
-		$('#cnt img').each(function () {
-			this.src.replace('chrome-extension:', a.protocol);
-		});
+		$('base').attr('href', a.protocol + '//' + a.hostname);
+		$('#cnt').html(response.parse.text['*']
+			.replace(new RegExp('href="/wiki/', 'g'), 'href="' + chrome.extension.getURL('show.html') + '?s=')
+			.replace(new RegExp('href="'+ url +'/wiki/', 'g'), 'href="' +chrome.extension.getURL('show.html'+ '?s=')));
+		$('#cnt').prepend('<h1>' + getUrlVar('s').replace(new RegExp('_', 'g'), ' ') + '</h1>\n');
 		$('#cnt a').each(function () {
 			var $this = $(this);
-			if (this.href.indexOf(base) != -1) {
-				this.href = 'show.html?s=' + $this.html();
-			} else {
-				$this.on("click", function (event) {
-					chrome.tabs.create({
-						url: $(this).attr('href')
-					});
+		$('#cnt a').not('#toc a').each(function () {
+			var $this = $(this),
+				href = this.href.split('/');
+			if ( href[0] != 'chrome-extension:' ) {
+			$(this).on("click", function (event) {
+				chrome.tabs.create({
+					url: $(this).attr('href')
 				});
-			}
+			});
+		}
 		});
-		$('#toc a').click(function() { return false; });
+		});
 	});
 });
