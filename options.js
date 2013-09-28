b8ff8b176e26b226b28105fbcc9d7d8282f2f1e5
David Knaack <david@knaack.me>
2013-09-28 12:28:52 +0200

    3.0.0

--- options.js
+++
@@ -1,8 +0,0 @@
-$(function ($) {
-	$('#content').submit(function () {
-		localStorage['wikiPrefix'] = $('#wikiURL').val();
-		location.replace('search.html');
-		return false;
-	});
-	$('#wikiURL').val(localStorage['wikiPrefix']);
-});
