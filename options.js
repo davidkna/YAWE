8f833116557120f89fd3cbc0afb4950c9eed0ecd
David Knaack <david@knaack.me>
2013-10-01 16:54:31 +0200

    add theme support & fix things

--- options.js
+++ options.js
@@ -1,14 +1,16 @@
 $(function ($) {
-	$('#content').submit(function () {
+	$('#content').submit(function (event) {
 
 		var wikiURL = $('#wikiURL').val().replace('https:', 'http:');
 		if (!wikiURL.endsWith('/')) {
 			wikiURL += '/';
 		}
 		localStorage['wikiURL'] = wikiURL;
+		localStorage['wikiTheme'] = $('#wikiTheme').val();
 
 		location.replace('index.html');
-		return false;
+		event.preventDefault();
 	});
 	$('#wikiURL').val(localStorage['wikiURL']);
+	$('#wikiTheme').val(localStorage['wikiTheme']);
 });
