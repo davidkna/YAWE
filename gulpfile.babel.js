8edaf58e180f63aa6217050cc9612d0a0760b33f
David Knaack <davidkna@users.noreply.github.com>
2016-01-26 16:19:59 +0100

    fix lint errors

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,9 +13,7 @@ import imagemin from 'gulp-imagemin'
 
 import sass from 'gulp-sass'
 
-gulp.task('clean', callback => {
-	return del('dist/', callback)
-})
+gulp.task('clean', callback => del('dist/', callback))
 
 gulp.task('release:chrome', ['clean'], () => {
 	gulp.run('chrome')
@@ -64,48 +62,52 @@ gulp.task('firefox', [
 	'html',
 ])
 
-gulp.task('generic', () => {
-	return gulp.src(['./src/generic/*', '!./src/generic/*.js'])
-	.pipe(gulp.dest('dist'))
-})
+gulp.task('generic', () =>
+	gulp
+		.src(['./src/generic/*', '!./src/generic/*.js'])
+		.pipe(gulp.dest('dist'))
+)
 
-gulp.task('html', () => {
-	return gulp.src('./src/*.html')
-	.pipe(htmlmin())
-	.pipe(gulp.dest('dist'))
-})
+gulp.task('html', () =>
+	gulp
+		.src('./src/*.html')
+		.pipe(htmlmin())
+		.pipe(gulp.dest('dist'))
+)
 
-gulp.task('img', () => {
-	return gulp.src([
-		'./src/images/icon_16x16.png',
-		'./src/images/icon_19x19.png',
-		'./src/images/icon_38x38.png',
-		'./src/images/icon_48x48.png',
-		'./src/images/icon_128x128.png',
-	])
-	.pipe(imagemin())
-	.pipe(gulp.dest('dist/images'))
-})
+gulp.task('img', () =>
+	gulp
+		.src([
+			'./src/images/icon_16x16.png',
+			'./src/images/icon_19x19.png',
+			'./src/images/icon_38x38.png',
+			'./src/images/icon_48x48.png',
+			'./src/images/icon_128x128.png',
+		])
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images'))
+)
 
 
 gulp.task('js:options', () => {
-	return browserify('./src/js/options.js')
-	.bundle()
-	.pipe(source('options.js'))
-	.pipe(gulp.dest('./dist/js/'))
+	browserify('./src/js/options.js')
+		.bundle()
+		.pipe(source('options.js'))
+		.pipe(gulp.dest('./dist/js/'))
 })
-gulp.task('js:app', () => {
-	return browserify('./src/js/app.js')
-	.bundle()
-	.pipe(source('app.js'))
-	.pipe(gulp.dest('./dist/js/'))
-})
+gulp.task('js:app', () =>
+	browserify('./src/js/app.js')
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('./dist/js/'))
+)
 
-gulp.task('js:plugins', () => {
-	return gulp.src('vendor/awesomplete/awesomplete.js')
-	.pipe(concat('plugins.js'))
-	.pipe(gulp.dest('./dist/js/'))
-})
+gulp.task('js:plugins', () =>
+	gulp
+		.src('vendor/awesomplete/awesomplete.js')
+		.pipe(concat('plugins.js'))
+		.pipe(gulp.dest('./dist/js/'))
+)
 
 function scss(browsers) {
 	return gulp.src('vendor/bootswatch/**/style.scss')
@@ -122,29 +124,30 @@ function scss(browsers) {
 }
 
 
-gulp.task('scss:web', () => {
-	return scss(['last 2 versions', 'Firefox ESR'])
-	.pipe(gulp.dest('dist/bootswatch/'))
-})
+gulp.task('scss:web', () =>
+	scss(['last 2 versions', 'Firefox ESR'])
+		.pipe(gulp.dest('dist/bootswatch/'))
+)
 
-gulp.task('scss:firefox', () => {
-	return scss(['Firefox ESR'])
-	.pipe(gulp.dest('dist/bootswatch/'))
-})
+gulp.task('scss:firefox', () =>
+	scss(['Firefox ESR'])
+		.pipe(gulp.dest('dist/bootswatch/'))
+)
 
-gulp.task('scss:chrome', () => {
-	return scss(['last 2 Chrome versions'])
-	.pipe(gulp.dest('dist/bootswatch/'))
-})
+gulp.task('scss:chrome', () =>
+	scss(['last 2 Chrome versions'])
+		.pipe(gulp.dest('dist/bootswatch/'))
+)
 
-gulp.task('scss:opera', () => {
-	return scss(['last 2 Opera versions'])
-	.pipe(gulp.dest('dist/bootswatch/'))
-})
+gulp.task('scss:opera', () =>
+	scss(['last 2 Opera versions'])
+		.pipe(gulp.dest('dist/bootswatch/'))
+)
 
-gulp.task('lint', () => {
-	return gulp.src(['**/*.js', '!node_modules/**', '!vendor/**', '!dist/**'])
+gulp.task('lint', () =>
+	gulp
+		.src(['**/*.js', '!node_modules/**', '!vendor/**', '!dist/**'])
 		.pipe(eslint())
     .pipe(eslint.format())
 		.pipe(eslint.failAfterError())
-})
+)
