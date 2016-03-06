63f18db01e732c2de2cfcd55cb9e0b5f7aa5624b
David Knaack <davidkna@users.noreply.github.com>
2016-03-06 18:29:15 +0100

    don't wait for DOMContentLoaded

--- src/js/app.js
+++ src/js/app.js
@@ -11,72 +11,70 @@ import {
 
 import { default as Awesomplete } from 'awesomplete'
 
-document.addEventListener('DOMContentLoaded', () => {
-  initDomElems()
-  const { $base, $search, $content } = domElems
+initDomElems()
+const { $base, $search, $content } = domElems
 
-  $('link').setAttribute('href', `bootswatch/${options.theme}/style.css`)
-  loadFromHash()
+$('link').setAttribute('href', `bootswatch/${options.theme}/style.css`)
+loadFromHash()
 
-  $('#back').addEventListener('click', (event) => {
-    event.preventDefault()
-    history.back()
+$('#back').addEventListener('click', (event) => {
+  event.preventDefault()
+  history.back()
+})
+$('#forward').addEventListener('click', (event) => {
+  event.preventDefault()
+  history.forward()
+})
+$('#newTab').addEventListener('click', (event) => {
+  event.preventDefault()
+  window.open(`${options.url}wiki/${$('#search').value}`, '_newtab')
+})
+
+addEventListener('hashchange', loadFromHash)
+
+const awesome = new Awesomplete($('#search'), {
+  minChars: 1,
+  maxItems: 10,
+})
+
+$search.addEventListener('input', () => {
+  search($search.value, (response) => {
+    awesome.list = response
+    awesome.evaluate()
   })
-  $('#forward').addEventListener('click', (event) => {
-    event.preventDefault()
-    history.forward()
-  })
-  $('#newTab').addEventListener('click', (event) => {
-    event.preventDefault()
-    window.open(`${options.url}wiki/${$('#search').value}`, '_newtab')
-  })
+})
 
-  addEventListener('hashchange', loadFromHash)
+$content.addEventListener('click', (event) => {
+  const target = findParentLink(event.target)
+  if (target) {
+    if (isWikiUrl(target.href)) {
+      event.preventDefault()
+      const searchTerm = articleNameFromUrl(target.href)
 
-  const awesome = new Awesomplete($('#search'), {
-    minChars: 1,
-    maxItems: 10,
-  })
-
-  $search.addEventListener('input', () => {
-    search($search.value, (response) => {
-      awesome.list = response
-      awesome.evaluate()
-    })
-  })
-
-  $content.addEventListener('click', (event) => {
-    const target = findParentLink(event.target)
-    if (target) {
-      if (isWikiUrl(target.href)) {
+      $search.value = searchTerm
+      $content.scrollTop = 0
+      getArticle(searchTerm)
+    } else {
+      if (target.dataset.internal !== '') {
         event.preventDefault()
-        const searchTerm = articleNameFromUrl(target.href)
-
-        $search.value = searchTerm
-        $content.scrollTop = 0
-        getArticle(searchTerm)
-      } else {
-        if (target.dataset.internal !== '') {
-          event.preventDefault()
-          window.open(target.href, '_newtab')
-        }
+        window.open(target.href, '_newtab')
       }
     }
-  })
+  }
+})
 
-  $('a[href="options.html"]').addEventListener('click', () => {
-    $base.setAttribute('href', '')
-  })
+$('a[href="options.html"]').addEventListener('click', () => {
+  $base.setAttribute('href', '')
+})
 
-  $('.dropdown .btn').addEventListener('click', (event) => {
-    event.preventDefault()
-    event.target.classList.toggle('active')
+$('.dropdown .btn').addEventListener('click', (event) => {
+  event.preventDefault()
+  event.target.classList.toggle('active')
 
-    $('.dropdown-menu').classList.toggle('show')
-  })
+  $('.dropdown-menu').classList.toggle('show')
+})
 
-  $('#search-form').addEventListener('submit', (event) => {
-    event.preventDefault()
-    getArticle($search.value)
-  })
+$('#search-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  getArticle($search.value)
 })
