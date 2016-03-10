a5d4abf46ed6223ac6eabdbce955031d2ef26d08
David Knaack <davidkna@users.noreply.github.com>
2016-03-10 19:43:11 +0100

    specify disabled rule

--- src/js/options.js
+++ src/js/options.js
@@ -1,16 +1,18 @@
 import { $, http2https, options, timestamp } from './helper'
 
 (() => {
+  // get <select> value
   function getVal(el) {
     return el
       .options[el.selectedIndex]
       .getAttribute('value')
   }
 
+  // set <select> value
   function setVal(el, val) {
     for (let i = 0; i < el.options.length; i++) {
       if (el.options[i].value === val) {
-        el.selectedIndex = i // eslint-disable-line
+        el.selectedIndex = i // eslint-disable-line no-param-reassign
         return
       }
     }
@@ -18,6 +20,7 @@ import { $, http2https, options, timestamp } from './helper'
     setVal(el, 'custom')
   }
 
+  // Save on submit
   $('form', $('#content')).addEventListener('submit', (event) => {
     event.preventDefault()
     let url = http2https($('#url').value)
@@ -40,18 +43,25 @@ import { $, http2https, options, timestamp } from './helper'
     location.replace('app.html')
   })
 
+  // Theme preview
   $('#theme').addEventListener('change', () => {
     $('link').setAttribute('href', `bootswatch/${getVal($('#theme'))}/style.css`)
   })
 
+  // Set initial values
   $('#url').setAttribute('value', options.url)
   setVal($('#theme'), options.theme)
+
+  // Load theme
   $('link').setAttribute('href', `bootswatch/${options.theme}/style.css`)
 
+  // Back...
   $('.icon-left-open').addEventListener('click', (event) => {
     event.preventDefault()
     history.back()
   })
+
+  // fix url to https on blur
   $('#url').addEventListener('blur', (event) => {
     $('#url').value = http2https(event.target.value)
   })
