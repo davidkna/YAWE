63cb439c567245e54bfc76cd035891fa9970a6ec
David Knaack <davidkna@users.noreply.github.com>
2016-04-08 17:11:52 +0200

    query string stuff

--- src/js/helper/query-string.js
+++ src/js/helper/query-string.js
@@ -1,22 +1,37 @@
+function decode(str) {
+  // jscs:disable disallowArrayDestructuringReturn
+  const [key, value] = str.split('=').map(decodeURIComponent)
+
+  return { key, value }
+}
+
 // Transforms query string to javascript object
 export function fromQueryString(str) {
   return str
     .replace(/^[\?#]/, '')
     .split('&')
     .reduce((result, current) => {
-      // jscs:disable disallowArrayDestructuringReturn
-      const [key, value] = current.split('=')
+      const { key, value } = decode(current)
 
-      const _result = result
-      _result[key] = decodeURIComponent(value)
-      return _result
+      result[key] = value // eslint-disable-line no-param-reassign
+      return result
     }, {})
+}
+
+function encode(k, v) {
+  const key = encodeURIComponent(k)
+  const value = decodeURIComponent(v)
+
+  return { key, value }
 }
 
 // Transforms Javascript Object to query string
 export function toQueryString(obj) {
   return Object
     .keys(obj)
-    .map(key => `${key}=${encodeURIComponent(obj[key])}`)
+    .map(_key => {
+      const { key, value } = encode(_key, obj[_key])
+      return `${key}=${value}`
+    })
     .join('&')
 }
